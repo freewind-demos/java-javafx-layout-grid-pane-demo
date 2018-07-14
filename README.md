535a058b114b6a5322abe0e7ccbe89c0f36ba696
freewind <nowindlee@gmail.com>
2018-07-14 17:55:28 +0800

    gridpane demo

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-JavaFX Hello World Demo
-=======================
+JavaFX Layout GridPane Demo
+===========================
 
-Show a window with `Hello, JavaFX` label.
+Layout -> `GridPane`
 
--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,9 +1,11 @@
 package demo;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Hello extends Application {
@@ -14,10 +16,30 @@ public class Hello extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello");
-        StackPane root = new StackPane() {{
-            getChildren().add(new Label("Hello, JavaFX!"));
+        GridPane root = new GridPane() {{
+            setHgap(10);
+            setVgap(10);
+            setPadding(new Insets(0, 10, 0, 10));
+
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    add(cell(i + "," + j), i, j);
+                }
+            }
+
+            add(cell("7, 8, 3, 3"), 7, 8, 3, 3);
+            add(cell("8, 2, 3, 3"), 8, 2, 3, 3);
         }};
-        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
+    }
+
+    private Pane cell(final String label) {
+        return new Pane() {{
+            setStyle("-fx-background-color: RED");
+            setPrefWidth(50);
+            setPrefHeight(50);
+            getChildren().add(new Label(label));
+        }};
     }
 }
